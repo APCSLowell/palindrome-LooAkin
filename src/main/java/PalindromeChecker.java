52317b43161145876d0ae15c09bb9ae5cd3f21a7
LooAkin <143376985+LooAkin@users.noreply.github.com>
2024-01-29 12:54:34 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -23,7 +23,7 @@ public void tester()
         e.printStackTrace();
     }
   System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
+  for (int i=0; i < lines.length(); i++) 
   {
     if(palindrome(lines[i])==true)
     {
@@ -37,15 +37,20 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  if(reverse(word).equalsword)
-    return true;
-  else
-    return false;
+    String charOnly = "";
+    for(int i = 0; i < word.length(); i++){
+      if(char.isLetter(word.charAt(i))
+        charOnly = charOnly + word.substring(i, i+1);
+    }
+    if(reverse(word).equals(word))
+      return true;
+    else 
+        return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    for(int i = str.length-1; i >= 0; i++)
+    for(int i = str.length()-1; i >= 0; i++)
       sNew = sNew + str.substring(i, i+1);
     return sNew;
 }

e9af523c4ca8f9db5c65659f8b05ed5d2914a35b
LooAkin <143376985+LooAkin@users.noreply.github.com>
2024-01-29 12:56:08 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -39,7 +39,7 @@ public boolean palindrome(String word)
 {
     String charOnly = "";
     for(int i = 0; i < word.length(); i++){
-      if(char.isLetter(word.charAt(i))
+      if(char.isLetter(word.charAt(i)==true)
         charOnly = charOnly + word.substring(i, i+1);
     }
     if(reverse(word).equals(word))

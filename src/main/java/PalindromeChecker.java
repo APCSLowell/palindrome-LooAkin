eb7c7008fb88cf105fb215b063872763d5a94389
LooAkin <143376985+LooAkin@users.noreply.github.com>
2024-01-31 12:06:27 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -57,7 +57,7 @@ public boolean palindrome(String word)
 }
 public String reverse(String str)
 {
-  String sNew = new String;
+  String sNew = new String();
     for(int i = str.length()-1; i >= 0; i++)
       sNew = sNew + str.substring(i, i+1);
     return sNew;
